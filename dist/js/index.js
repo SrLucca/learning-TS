632665591d7587f6562a9a7bbe043106f1d19e9d
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-08-28 18:33:49 -0300

    transcricao do ts para js

---
+++ dist/js/index.js
@@ -0,0 +1,42 @@
+"use strict";
+//tsc --init -> informar q aq é um projeto ts -> cria tsconfig.json
+//tsc -> compila a pasta src e os arquivos ts
+//tsc -w -> compilacao automatica
+const x = 10; //criacao de variavle igual do js
+//:tipo_variavel -> padronizacao -> tipagem ts(string,number...)
+console.log(x);
+//inferencia x annotation
+let y = 12; //inferencia
+//let y:number = 12; -> annotation -> mais explicito
+// String -> S maiusculo -> objeto
+// string -> s minusculo -> tipo de dado
+//array
+const numeros = [1, 2, 3];
+console.log(numeros);
+console.log(numeros.length); //= len
+numeros.push(5); //adicionar a lista
+//tuplas
+let tupla; //diferenca q cada index é antes tipado, mas é modificavel
+tupla = [2, "teste", ["oi", "tchau"]];
+console.log(tupla);
+tupla.push(10); //adicionando o 10 msm sem ter informado
+console.log(tupla);
+//objects literals -> {prop: value} -> dicionario
+const user = {
+    name: "Lucca",
+    age: 20,
+};
+console.log(user);
+console.log(user.age);
+//any -> valor q aceita qualquer tipo de dado -> tipo *args
+let a = 0;
+a = "teste";
+console.log(a);
+a = true;
+console.log(a);
+a = 10;
+console.log(a);
+//union type -> uniao de tipos
+let id = "10";
+id = 200;
+const userid = 10;
